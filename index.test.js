45bda8a8b29eb3e7292f8f3e5ced30563d175444
Breno Alves <breno.alvs@hotmail.com>
2020-11-20 00:14:36 -0300

    Ignoring git command errors

--- index.test.js
+++ index.test.js
@@ -1,7 +1,3 @@
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   console.log('testing')
