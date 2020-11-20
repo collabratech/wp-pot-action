7c7013dfeeee4cc382842313d0f2553f1cbf226e
Breno Alves <breno.alvs@hotmail.com>
2020-11-19 21:23:14 -0300

    Added placeholder test

--- index.test.js
+++ index.test.js
@@ -4,7 +4,5 @@ const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+  console.log('testing')
 })
